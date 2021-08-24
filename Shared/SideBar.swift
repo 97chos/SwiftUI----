53c1776e9b8201cfbcd8a41d17478a825876432c
sangho.Cho <sangho.cho@flitto.com>
2021-08-24 19:00:32 +0900

    [5] macOS 앱 분기 처리

--- Shared/SideBar.swift
+++ Shared/SideBar.swift
@@ -10,21 +10,32 @@ import SwiftUI
 struct SideBar: View {
     var body: some View {
       NavigationView {
-        List {
-          NavigationLink(destination: CoursesView()) {
-            Label("Courses", systemImage: "book.closed")
-          }
-          Label("Tutorials", systemImage: "list.bullet.rectangle")
-          Label("LiveStreams", systemImage: "tv")
-          Label("Certificates", systemImage: "mail.stack")
-          Label("Search", systemImage: "magnifyingglass")
-        }
-        .listStyle(SidebarListStyle())
-        .navigationTitle("Learn")
+        #if os(iOS)
+        content
+          .navigationTitle("Learn")
+        #else
+        content
+          .frame(minWidth: 200, idealWidth: 250, maxWidth: 300)
+        #endif
 
+        // default View: iPadOS에서 화면 진입 시 SideBar와 함께 보여지는 기본 뷰
         CoursesView()
       }
     }
+
+  var content: some View {
+    List {
+      NavigationLink(destination: CoursesView()) {
+        Label("Courses", systemImage: "book.closed")
+      }
+      Label("Tutorials", systemImage: "list.bullet.rectangle")
+      Label("LiveStreams", systemImage: "tv")
+      Label("Certificates", systemImage: "mail.stack")
+      Label("Search", systemImage: "magnifyingglass")
+        .listStyle(SidebarListStyle())
+
+    }
+  }
 }
 
 struct SideBar_Previews: PreviewProvider {
