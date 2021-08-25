ebda48667440a256198c1581074edca54ae72ae2
sangho.Cho <sangho.cho@flitto.com>
2021-08-25 12:17:47 +0900

    [7] showView Transition 구현

--- Shared/CourseList.swift
+++ Shared/CourseList.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct CourseList: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @ViewBuilder
+  var body: some View {
+    #if os(iOS)
+    content
+      .listStyle(InsetGroupedListStyle())
+    #else
+    content
+      .frame(minWidth: 800, minHeight: 600)
+    #endif
+  }
+  var content: some View {
+    List(0 ..< 20) { item in
+      CourseRow()
     }
+    .navigationTitle("Courses")
+  }
 }
 
 struct CourseList_Previews: PreviewProvider {
-    static var previews: some View {
-        CourseList()
-    }
+  static var previews: some View {
+    CourseList()
+  }
 }
+
--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -8,20 +8,31 @@
 import SwiftUI
 
 struct CoursesView: View {
+  @State var show = false
+
   var body: some View {
-    #if os(iOS)
-    content
-      .listStyle(InsetGroupedListStyle())
-    #else
-    content
-      .frame(minWidth: 800, minHeight: 600)
-    #endif
-  }
-  var content: some View {
-    List(0 ..< 20) { item in
-      CourseRow()
+    ZStack {
+      CourseItem()
+        .frame(width: 335, height: 250)
+      // zIndex를 사용하여 뷰의 순서르 변경하는 것보다 VStack을 사용하는 것이 더 자연스러움
+      VStack {
+        if show {
+          CourseItem()
+            .transition(.move(edge: .leading))
+            // safeArea 무시
+            .edgesIgnoringSafeArea(.all)
+        }
+      }
     }
-    .navigationTitle("Courses")
+    // 프리뷰에서는 노출 애니메이션이 제대로 동작하지 않으나, 앱에서는 정상적으로 동작됨
+    .onTapGesture {
+      // 두 애니메이션 방법 모두 사용 가능하나, 기하학적인 애니메이션은 이 방법이 더 적합하다고 판단,
+      // 애니메이션 도중 여러번 터치해도 부드럽게 동작됨.
+      withAnimation(.spring()) {
+        show.toggle()
+      }
+    }
+//    .animation(.spring())
   }
 }
 
