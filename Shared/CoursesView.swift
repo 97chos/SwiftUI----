703e6853784cafdd72cc14e03f7064511f3f6f14
sangho.Cho <sangho.cho@flitto.com>
2021-08-25 14:53:58 +0900

    [8] matchedGeometryEffect 적용

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -9,25 +9,41 @@ import SwiftUI
 
 struct CoursesView: View {
   @State var show = false
+  @Namespace var nameSpace
 
   var body: some View {
     ZStack {
+      // 작은 뷰
       CourseItem()
+        // 연결되는 뷰의 기하학적인 구조만을 정의, frame 이전에 설정
+        .matchedGeometryEffect(id: "Card", in: nameSpace, isSource: !show)
         .frame(width: 335, height: 250)
-      // zIndex를 사용하여 뷰의 순서르 변경하는 것보다 VStack을 사용하는 것이 더 자연스러움
-      VStack {
-        if show {
+
+      if show {
+        ScrollView {
+          // 큰 뷰
           CourseItem()
-            .transition(.move(edge: .leading))
-            // safeArea 무시
-            .edgesIgnoringSafeArea(.all)
+            .matchedGeometryEffect(id: "Card", in: nameSpace)
+            .frame(height: 300)
+            .transition(.opacity)
+
+          VStack {
+            ForEach(0 ..< 20) { item in
+              CourseRow()
+            }
+          }
+          .padding()
         }
+        .transition(.opacity)
+        // safeArea 무시
+        .edgesIgnoringSafeArea(.all)
       }
     }
     // 프리뷰에서는 노출 애니메이션이 제대로 동작하지 않으나, 앱에서는 정상적으로 동작됨
     .onTapGesture {
       // 두 애니메이션 방법 모두 사용 가능하나, 기하학적인 애니메이션은 이 방법이 더 적합하다고 판단,
       // 애니메이션 도중 여러번 터치해도 부드럽게 동작됨.
+      // .animation을 사용할 경우 애니메이션이 느려지고, 스크롤 시 렉이 발생한다.
       withAnimation(.spring()) {
         show.toggle()
       }
