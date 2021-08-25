aff72a040fe881019ed8a559e09fd8ff2c258b54
sangho.Cho <sangho.cho@flitto.com>
2021-08-25 16:18:44 +0900

    [10] 데이터 바인딩

---
+++ Shared/Course.swift
@@ -0,0 +1,20 @@
+//
+//  Course.swift
+//  DesignCodCourse
+//
+//  Created by 조상호 on 2021/08/25.
+//
+
+import SwiftUI
+
+struct Course: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Course_Previews: PreviewProvider {
+    static var previews: some View {
+        Course()
+    }
+}
