ce7bf1537e5c4c290c038552f7ddae76ae306175
sangho.Cho <sangho.cho@flitto.com>
2021-08-27 10:39:59 +0900

    [13] matched Container 및 zIndex 사용

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -16,40 +16,61 @@ struct CoursesView: View {
   var body: some View {
     ZStack {
       // 작은 뷰
-      ScrollView() {
-        LazyVGrid(
-          columns: [GridItem(.adaptive(minimum: 160), spacing: 16)],
-          spacing: 16
-        ) {
+      ScrollView {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 160), spacing: 16)],
+                  spacing: 16) {
           ForEach(courses) { item in
-            CourseItem(course: item)
-              // 연결되는 뷰의 기하학적인 구조만을 정의, frame 이전에 설정
-              .matchedGeometryEffect(id: item.id, in: nameSpace, isSource: !show)
-              .frame(height: 200)
-              .onTapGesture {
-                // 두 애니메이션 방법 모두 사용 가능하나, 기하학적인 애니메이션은 이 방법이 더 적합하다고 판단,
-                // 애니메이션 도중 여러번 터치해도 부드럽게 동작됨.
-                // .animation을 사용할 경우 애니메이션이 느려지고, 스크롤 시 렉이 발생한다.
-                withAnimation(.spring()) {
-                  show.toggle()
-                  selectedItem = item
-                  isDisable = true
+            VStack {
+              CourseItem(course: item)
+                // 연결되는 뷰의 기하학적인 구조만을 정의, frame 이전에 설정
+                .matchedGeometryEffect(id: item.id, in: nameSpace, isSource: !show)
+                .frame(height: 200)
+                .onTapGesture {
+                  // 두 애니메이션 방법 모두 사용 가능하나, 기하학적인 애니메이션은 이 방법이 더 적합하다고 판단,
+                  // 애니메이션 도중 여러번 터치해도 부드럽게 동작됨.
+                  // .animation을 사용할 경우 애니메이션이 느려지고, 스크롤 시 렉이 발생한다.
+                  withAnimation(.spring(response: 0.4, dampingFraction: 0.8, blendDuration: 0)) {
+                    show.toggle()
+                    selectedItem = item
+                    isDisable = true
+                  }
                 }
-              }
-              .disabled(isDisable)
+                .disabled(isDisable)
+            }
+            .matchedGeometryEffect(id: "container\(item.id)", in: nameSpace, isSource: !show)
           }
         }
-        .padding(16)
+        .padding(10)
         .frame(maxWidth: .infinity)
       }
-
+      .zIndex(1)
       if selectedItem != nil {
-        ScrollView {
-          // 큰 뷰
-          CourseItem(course: selectedItem!)
-            .matchedGeometryEffect(id: selectedItem!.id, in: nameSpace)
-            .frame(height: 300)
-            .transition(.opacity)
+        ZStack(alignment: .topTrailing) {
+          VStack {
+            ScrollView {
+              // 큰 뷰
+              CourseItem(course: selectedItem!)
+                .matchedGeometryEffect(id: selectedItem!.id, in: nameSpace)
+                .frame(height: 300)
+                .transition(.opacity)
+
+
+              VStack {
+                ForEach(0 ..< 20) { item in
+                  CourseRow()
+                }
+              }
+              .padding()
+            }
+          }
+          .background(Color("Background 1"))
+          .clipShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
+          .matchedGeometryEffect(id: "container\(selectedItem!.id)", in: nameSpace)
+          // safeArea 무시
+          .edgesIgnoringSafeArea(.all)
+
+          CloseButton()
+            .padding(.trailing, 16)
             .onTapGesture {
               withAnimation(.spring()) {
                 show.toggle()
@@ -59,23 +80,8 @@ struct CoursesView: View {
                 }
               }
             }
-
-          VStack {
-            ForEach(0 ..< 20) { item in
-              CourseRow()
-            }
-          }
-          .padding()
         }
-        .background(Color("Background 1"))
-        .transition(
-          // 비대칭형 애니메이션 구현시 사용
-          .asymmetric(insertion: AnyTransition.opacity.animation(Animation.spring().delay(0.3)),
-                      removal: AnyTransition.opacity.animation(.spring())
-          )
-        )
-        // safeArea 무시
-        .edgesIgnoringSafeArea(.all)
+        .zIndex(2)
       }
     }
   }
